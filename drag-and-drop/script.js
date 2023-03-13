8e868dac7c4a749706dd2761fac90ca09158df1a
Hershell Koger <hersheykoger@yahoo.com>
2023-03-12 21:27:10 -0400

    Add forEach() method to ballContainer, then add eventListeners for all the drag events; functions created to handle each drag event, either adding or removing 'drag-over' class

--- drag-and-drop/script.js
+++ drag-and-drop/script.js
@@ -11,3 +11,26 @@ function dragStart(e) {
         e.target.classList.add('hide'), 0
     })
 }
+
+ballContainers.forEach(container => {
+    container.addEventListener('dragenter', dragEnter)
+    container.addEventListener('dragover', drageOver)
+    container.addEventListener('dragleave', dragLeave)
+    container.addEventListener('drop', drop)
+})
+
+function dragEnter(e) {
+    e.target.classList.add('drag-over')
+}
+
+function drageOver(e) {
+    e.target.classList.add('drag-over')
+}
+
+function dragLeave(e) {
+    e.target.classList.remove('drag-over')
+}
+
+function drop(e) {
+    e.target.classList.remove('drag-over')
+}
