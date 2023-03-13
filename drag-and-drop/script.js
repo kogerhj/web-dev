495f29b85a065debfb81f027bd26091d634f609d
Hershell Koger <hersheykoger@yahoo.com>
2023-03-12 20:53:33 -0400

    Add setTimeout() to dragStart() function to prevent ball from vanishing

--- drag-and-drop/script.js
+++ drag-and-drop/script.js
@@ -1,12 +1,13 @@
 const ball = document.querySelector('.ball')
-
 const ballId = document.getElementById('ball')
-
 const ballContainers = document.querySelectorAll('.box')
 
 ball.addEventListener('dragstart', dragStart)
 
 function dragStart(e) {
     e.dataTransfer.setData('text/plain', e.target.id)
-    e.target.classList.add('hide')
+
+    setTimeout(() => {
+        e.target.classList.add('hide'), 0
+    })
 }
