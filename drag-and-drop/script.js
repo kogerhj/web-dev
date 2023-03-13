62c3365c493a5a0c02324597b1448167b3da6e22
Hershell Koger <hersheykoger@yahoo.com>
2023-03-12 21:37:12 -0400

    Remove ballId binding from global scope

--- drag-and-drop/script.js
+++ drag-and-drop/script.js
@@ -1,5 +1,4 @@
 const ball = document.querySelector('.ball')
-const ballId = document.getElementById('ball')
 const ballContainers = document.querySelectorAll('.box')
 
 ball.addEventListener('dragstart', dragStart)
